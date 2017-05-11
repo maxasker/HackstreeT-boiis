7551bdcee73e23de63b59cec8cd36429e1674789
bostongurra2 <simme.norberg@gmail.com>
2017-05-12 00:59:02 +0200

    grejer

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".pulse-button").delay(2000).hide(1000)
+    $(".pulse-button-done").delay(2000).fadeIn(1000)
+    $(".pulse-button-done").delay(1000).hide(1000)
+    $("#traininfo").delay(5000).slideDown()
+});
