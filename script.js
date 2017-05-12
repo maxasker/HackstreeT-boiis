95d4758e4f715557f416ab87492cee33a18f6f5b
maxasker <maxasker@hotmail.com>
2017-05-12 04:11:56 +0200

    push

--- script.js
+++ script.js
@@ -5,10 +5,29 @@ $(document).ready(function(){
     $("#traininfo").delay(5000).slideDown()
 });
 
-$(".maxbut").click(function(){
-    $(".container").empty();
+
+var printInterval;
+var times = 0;
+var timesrun = 0;
+
+start();
+
+function start() {
+    function print() {
+        times += 1;
+        if (times == 2) {
+            $(".container").empty();
     $("h1").remove();
     $("<h1>Today</h1>").appendTo("#maxcenter");
     $("<img id="+'maximg'+" class="+'img'+'img-repsonsive'+" src="+"london.png"+">").appendTo("html");
     $("<img id="+'maximg2'+" class="+'img'+'img-repsonsive'+" src="+"london2.png"+">").appendTo("html");
-});
+            stop();
+            
+        }
+    }
+    printInterval = setInterval(print, 800);
+}
+
+function stop() {
+      clearInterval(printInterval);
+    }
