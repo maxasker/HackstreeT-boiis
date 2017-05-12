09ba367a354e1eda84465309f2dc7b54dc6f0fc3
maxasker <maxasker@hotmail.com>
2017-05-12 03:54:35 +0200

    pillat

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
     $(".pulse-button-done").delay(1000).hide(1000)
     $("#traininfo").delay(5000).slideDown()
 });
+
+$(".maxbut").click(function(){
+    $(".container").empty();
+    $("h1").remove();
+    $("<h1>Today</h1>").appendTo("#maxcenter");
+    $("<img id="+'maximg'+" class="+'img'+'img-repsonsive'+" src="+"london.png"+">").appendTo("html");
+    $("<img id="+'maximg2'+" class="+'img'+'img-repsonsive'+" src="+"london2.png"+">").appendTo("html");
+});
