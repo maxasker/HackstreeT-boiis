a47010ae33373d6ccf757de00eec6b44e86d3274
maxasker <maxasker@hotmail.com>
2017-05-12 03:08:48 +0200

    ändringar

--- Max/maxscript.js
+++ Max/maxscript.js
@@ -19,22 +19,30 @@ start();
 function start() {
     function print() {
         times += 1;
-        $("._" + times).show();
+        $("._" + times).fadeIn( 1000 );
         if (times == 10) {
             times = 0;
             hideshizzle();
             stop();
             
-        } 
-        if (times==6 && timesrun == 0){
+        }
+        if (times==4 && timesrun == 0){
             timesrun += 1;
             $(".firstdel").remove();
             $('<span class="col-sm-12 glyphicon glyphicon-ok"></span>').appendTo('.first');
+            $("#header").text("Carriage 1 (2)");
         }
-        if (times==9 && timesrun == 1){
+        if (times==7 && timesrun == 1){
             timesrun += 1;
             $(".seconddel").remove();
             $('<span class="col-sm-12 glyphicon glyphicon-ok"></span>').appendTo('.second');
+            $("#header").text("Carriage 1 (1)");
+        }
+        if (times==9 && timesrun == 2){
+            timesrun += 1;
+            $(".thirddel").remove();
+            $('<span class="col-sm-12 glyphicon glyphicon-ok"></span>').appendTo('.third');
+            $("#header").text("Carriage 1 clear");
         }
     }
     printInterval = setInterval(print, 800);
